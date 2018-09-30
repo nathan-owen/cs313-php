e404e615261e60bf64e777a487c0ca07d55fd451
Nathan Owen <nathanowen@byui.edu>
2018-09-29 21:45:13 -0500

    Changed Server Date/Time wording

--- web/header.php
+++ web/header.php
@@ -5,7 +5,7 @@ echo '
     <li class="active"><a href="index.php">Home</a></li>
     <li><a href="assignments.php">Assignments</a></li>
     <li class="right"> <a href="#">
-        Server Date: '. date("m/d/Y -- h:ia") . '
+        Server Date/Time: '. date("m/d/Y -- h:ia") . '
     </a>
     </li>
   </ul>
