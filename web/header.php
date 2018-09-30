0500caa12fc51b2d67e6ef8b0e7e4c25ea7cf9ba
Nathan Owen <nathanowen@byui.edu>
2018-09-29 21:42:27 -0500

    FIxed header issue

--- web/header.php
+++ web/header.php
@@ -5,7 +5,7 @@ echo '
     <li class="active"><a href="index.php">Home</a></li>
     <li><a href="assignments.php">Assignments</a></li>
     <li class="right"> <a href="#">
-        echo "Server Date: " . date("m/d/Y -- h:ia");
+        Server Date: '. date("m/d/Y -- h:ia") . '
     </a>
     </li>
   </ul>
