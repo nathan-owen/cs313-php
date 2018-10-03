e6ad094b052871957e841d5859555313b78cc0c2
Nathan Owen <nathanowen@byui.edu>
2018-10-02 19:56:13 -0500

    Team Activity

--- web/team03/test.php
+++ web/team03/test.php
@@ -10,8 +10,14 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $comments = $_POST['comments'];
 $major = $_POST['major'];
+$visitedContinents = $_POST['continents'];
 
 echo "<b>User Name:</b> " . $name . '<br>';
 echo "<b>Email: </b><a href='mailto:" . $email . "'>" . $email . "</a><br>";
 echo "<b>Major:</b> " . $major . '<br>';
 echo "<b>Comments:</b> " . $comments . '<br>';
+echo "<b> Visited Continents:</b><br>";
+
+foreach($visitedContinents as $selected) {
+    echo $selected ."<br>";
+}
