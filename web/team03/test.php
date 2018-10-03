9f050d1d5c5ab8b173c7ff95ebf37c4889715480
Nathan Owen <nathanowen@byui.edu>
2018-10-02 19:43:08 -0500

    Team Activity

--- web/team03/test.php
+++ web/team03/test.php
@@ -11,4 +11,7 @@ $email = $_POST['email'];
 $comments = $_POST['comments'];
 $major = $_POST['major'];
 
-echo $major;
+echo "User Name: " . $name . '\r';
+echo "<a href='mailto:" . $email . "'>" . $email . "</a>\r";
+echo "Major: " . $major . '\r';
+echo "Comments: " . $comments . '\r';
