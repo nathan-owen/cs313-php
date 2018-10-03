7a5199a91d6020e0b4819a910b3dbe6767afd88d
Nathan Owen <nathanowen@byui.edu>
2018-10-02 19:31:10 -0500

    Team ACtivity

---
+++ web/team03/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: natha
+ * Date: 10/2/2018
+ * Time: 7:25 PM
+ */
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comments = $_POST['comments'];
+
+echo $name;
+echo $email;
+echo $comments;
