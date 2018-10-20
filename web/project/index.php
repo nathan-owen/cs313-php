0f715999b039f20f89cf7182b984ece485481629
Nathan Owen <nathanowen@byui.edu>
2018-10-20 18:13:24 -0500

    project - week 5

--- web/project/index.php
+++ web/project/index.php
@@ -15,7 +15,7 @@ $db = connectToDatabase();
 <h3>Open Change Requests</h3>
     <?php
         $statement = $db->query("
-          SELECT tickets.id, title, dateSubmitted, users.name AS requestor, 
+          SELECT tickets.id, title, to_char(dateSubmitted, 'HH12:MI;SS') as dateSubmitted, users.name AS requestor, 
             state, status, approvedBy, dateUpdated 
           FROM tickets
           JOIN users ON users.id = tickets.requestor 
@@ -33,7 +33,7 @@ $db = connectToDatabase();
             foreach ($results as $ticket) {
                 echo '<tr>';
                 echo "<td><a href='request.php?id=" . $ticket['id'] . "'>" . $ticket['title'] . "</a></td>";
-                echo "<td>" . date('d-m-Y',strtotime($ticket['dateSubmitted'])). "</td>";
+                echo "<td>" . $ticket['dateSubmitted'] . "</td>";
                 echo "<td>" . $ticket['requestor'] . "</td>";
                 echo "<td>" . $ticket['state'] . "</td>";
                 echo "<td>" . $ticket['status'] . "</td>";
