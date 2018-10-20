51980114a10f0c991c51edb099a1b5a9600e5d90
Nathan Owen <nathanowen@byui.edu>
2018-10-20 17:57:22 -0500

    project - week 5

--- web/project/index.php
+++ web/project/index.php
@@ -14,8 +14,24 @@ $db = connectToDatabase();
 <main>
 <h3>Open Change Requests</h3>
     <?php
-        foreach ($db->query("SELECT id, title, dateSubmitted, requestor, state, status, approvedBy, dateUpdated FROM tickets WHERE status = 'Open'") as $row) {
-            echo $row['title'];
+        $statement = $db->query("SELECT id, title, dateSubmitted, requestor, state, status, approvedBy, dateUpdated FROM tickets WHERE status = 'Open'");
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+        if(count($results) > 0)
+        {
+            echo '<table>';
+            echo '<thead>';
+            echo '<tr>';
+                echo '<th>Title</th><th>Date Submitted</th><th>Requestor</th><th>State</th><th>Status</th><th>Approved By</th><th>Date Updated</th>';
+            echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+            foreach ($results as $ticket) {
+                echo '<tr>';
+                echo "<td>$ticket['title']</td>";
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
         }
     ?>
 </main>
