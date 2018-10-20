5d7d7f1bbbdefc9ffce7befdf6c2e70886434450
Nathan Owen <nathanowen@byui.edu>
2018-10-20 18:20:09 -0500

    project - week 5

--- web/project/index.php
+++ web/project/index.php
@@ -33,12 +33,12 @@ $db = connectToDatabase();
             foreach ($results as $ticket) {
                 echo '<tr>';
                 echo "<td><a href='request.php?id=" . $ticket['id'] . "'>" . $ticket['title'] . "</a></td>";
-                echo "<td>" .date("m-d-Y h:i A", strtotime($ticket['dateSubmitted'])) . "</td>";
+                echo "<td>" . date("m-d-Y h:i A", strtotime($ticket['dateSubmitted'])) . "</td>";
                 echo "<td>" . $ticket['requestor'] . "</td>";
                 echo "<td>" . $ticket['state'] . "</td>";
                 echo "<td>" . $ticket['status'] . "</td>";
                 echo "<td>" . $ticket['approvedBy'] . "</td>";
-                echo "<td>" . $ticket['dateUpdated'] . "</td>";
+                echo "<td>" . date("m-d-Y h:i A", strtotime($ticket['dateUpdated'])) . "</td>";
                 echo '</tr>';
             }
             echo '</tbody>';
